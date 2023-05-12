0773e3392008ebfb011d5f4c23adf301f86c4458
ShootingWong <wangshuting@ruc.edu.cn>
2023-05-12 22:54:33 +0800

    mv py

--- RankModel_pretower_contra.py
+++ RankModel_pretower_contra.py
@@ -1,19 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
-from graph_embed_speed_nobert_distri import Embed
 from transformers import BertModel, BertTokenizer
 import pickle
-#from graph_embed_speed_newattn_768 import Embed
 import copy
 
-# class BCE_Loss_my(nn.Module):
-#     def __init__(self):
-#         super(BCE_Loss_my, self).__init__()
-#         # self.sigmod = torch.nn.Sigmoid()
-#     def forward(self, logits, batch_y):
-#         y_pred = torch.sigmoid(logits)
-#         return -(batch_y * torch.log(y_pred) + (1 - batch_y) * torch.log(1 - y_pred)).mean()
 
 import pynvml
 '''
@@ -31,32 +22,6 @@ def get_gpu():
     meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
     print('GPU USED : ', meminfo.used/1024/1024, 'MB')
 
-def id2str(ids):
-    return ' '.join([recab[id_] for id_ in ids])
-    
-def get_q_d(input_ids):
-    EOS = 30522
-    raw = copy.deepcopy(input_ids)
-    length = len(raw)
-    input_ids.reverse()
-    ind1 = input_ids.index(EOS)
-    input_ids = input_ids[ind1+1:]
-    ind2 = input_ids.index(EOS)
-    input_ids = input_ids[ind2+1:]
-    ind3 = input_ids.index(EOS)
-    
-    ind3 += ind2+1
-    ind2 += ind1+1
-    ind3 += ind1+1
-    
-    ind1 = length-1-ind1
-    ind2 = length-1-ind2
-    ind3 = length-1-ind3
-    
-    q_ids = raw[ind3+1: ind2]
-    d_ids = raw[ind2+1: ind1]
-    
-    return q_ids, d_ids
 
     
 class Ranker_nobert(nn.Module):
@@ -101,57 +66,3 @@ class Ranker_nobert(nn.Module):
         return loss#.squeeze(1)
         
         
-class BertRanker(nn.Module):
-    def __init__(self, bert_model, config):
-        super(BertRanker, self).__init__()
-        self.graph_emb_size = config["embsize"]
-        self.bert_model = bert_model
-        self.classifier = nn.Linear(768, 1)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.1)
-        init.xavier_normal_(self.classifier.weight)
-
-        #self.graph_ember = Embed(config)
-
-    def forward(self, batch_data):
-        """
-        Args:
-            input_ids ([type]): [description]
-            attention_mask ([type]): [description]
-            token_type_ids ([type]): [description]
-        """
-        #sequence modelling
-
-        # print('for batch data')
-        # for key in batch_data:
-        #     print('BATCH KEY = {}, DATA SIZE = {}'.format(key, batch_data[key].size()))
-
-        input_ids = batch_data["input_ids"]
-        attention_mask = batch_data["attention_mask"]
-        token_type_ids = batch_data["token_type_ids"]
-
-        bert_inputs = {'input_ids': input_ids, 'attention_mask': attention_mask, 'token_type_ids': token_type_ids}
-        sent_rep = self.dropout(self.bert_model(**bert_inputs)[1])
-
-        #graph modelling
-        #qid = batch_data["qid"]
-        #did = batch_data["did"]
-
-        #batch_size = qid.size(0)
-        #node_ids = torch.cat([qid, did], dim=0)
-
-        # print('BEFORE GRAPH')
-        # get_gpu()
-        #node_embeds = self.graph_ember(node_ids)
-
-        # print('AFTER GRAPH')
-        # get_gpu()
-        #q_graph_emb = node_embeds[:batch_size]
-        #d_graph_emb = node_embeds[batch_size:]
-
-        #graph_rep = q_graph_emb * d_graph_emb  # batch, input_dim
-
-        #classify
-        y_pred = self.classifier(sent_rep)
-
-        return y_pred.squeeze(1)
